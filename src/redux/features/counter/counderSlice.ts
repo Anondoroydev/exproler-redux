096ffca443e5abe47ec4e4b4e187ab172beda60e
Ajoy Sarkar (Anondo) <ajoysarker553@gmail.com>
2024-03-30 18:00:00 +0600

    commit

--- src/redux/features/counter/counderSlice.ts
+++ src/redux/features/counter/counderSlice.ts
@@ -4,11 +4,12 @@ const initialState = {
   count: 0,
 };
 
+
 const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    increment: (state, action) => { 
+    increment: (state, action) => {
       state.count = state.count + action.payload;
     },
     decrement: (state) => {
